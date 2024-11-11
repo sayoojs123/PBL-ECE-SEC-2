8a6158c4d8281df0a8a1bf35a6c701e1c4eedb74
sayoojs123 <166235142+sayoojs123@users.noreply.github.com>
2024-11-11 19:14:11 +0530

    Update main.py

--- AIML/Drowsing detection - AIML/main.py
+++ AIML/Drowsing detection - AIML/main.py
@@ -1,20 +1,10 @@
-# Importing OpenCV Library for basic image processing functions
 import cv2
-# Numpy for array related functions
 import numpy as np
-# Dlib for deep learning based Modules and face landmark detection
 import dlib
-# face_utils for basic operations of conversion
 from imutils import face_utils
-
-# Initializing the camera and taking the instance
 cap = cv2.VideoCapture(0)
-
-# Initializing the face detector and landmark detector
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
-
-# status marking for current state
 sleep = 0
 drowsy = 0
 active = 0
@@ -30,7 +20,6 @@ def blinked(a, b, c, d, e, f):
     down = compute(a, f)
     ratio = up / (2.0 * down)
 
-    # Checking if it is blinked
     if (ratio > 0.25):
         return 2
     elif (ratio > 0.21 and ratio <= 0.25):
@@ -43,8 +32,8 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     faces = detector(gray)
-    face_frame = frame.copy()  # Define face_frame before the loop
-    # detected face in faces array
+    face_frame = frame.copy()  
+
     for face in faces:
         x1 = face.left()
         y1 = face.top()
@@ -56,13 +45,12 @@ while True:
         landmarks = predictor(gray, face)
         landmarks = face_utils.shape_to_np(landmarks)
 
-        # The numbers are actually the landmarks which will show eye
+   
         left_blink = blinked(landmarks[36], landmarks[37],
                              landmarks[38], landmarks[41], landmarks[40], landmarks[39])
         right_blink = blinked(landmarks[42], landmarks[43],
                               landmarks[44], landmarks[47], landmarks[46], landmarks[45])
 
-        # Now judge what to do for the eye blinks
         if (left_blink == 0 or right_blink == 0):
             sleep += 1
             drowsy = 0
@@ -96,7 +84,7 @@ while True:
     cv2.imshow("Frame", frame)
     cv2.imshow("Result of detector", face_frame)
     key = cv2.waitKey(1)
-    if key == 27:  # Exit when 'ESC' key is pressed
+    if key == 27:  
         break
 
 cap.release()
